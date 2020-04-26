45eaaea26f7c12ac9d7773f0a9aec1dde6725780
Krystal Mitchell <10262399@my.uvu.edu>
2020-04-25 21:32:42 -0600

    client not product

--- src/nexusModels.js
+++ src/nexusModels.js
@@ -1,18 +1,16 @@
-import { objectType } from 'nexus'
+import { objectType } from "nexus";
 
 const Client = objectType({
-	name: 'Product',
-	definition(t) {
-		t.model.id()
-		t.model.name()
-		t.model.content()
-		t.model.phone()
-		t.model.interestLevel()
-		t.model.createdAt()
-		t.model.updatedAt()
-	}
-})
+  name: "Client",
+  definition(t) {
+    t.model.id();
+    t.model.name();
+    t.model.content();
+    t.model.phone();
+    t.model.interestLevel();
+    t.model.createdAt();
+    t.model.updatedAt();
+  }
+});
 
-export const Models = [
-	Client
-]
+export const Models = [Client];
